c534460d0076224a44d956a8b7b8b219423847b8
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-02-23 23:36:49 +0100

    adds 1 part HW 7
    how to remove keyboard listener?

---
+++ js/01-gallery.js
@@ -0,0 +1,63 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryPerentEl = document.querySelector(".gallery");
+const galleryItemsMarkup = createGallaryItemsMarkup(galleryItems);
+
+galleryPerentEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+galleryPerentEl.addEventListener("click", getOriginalImg);
+
+function createGallaryItemsMarkup() {
+  const galleryItemMerkup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>`
+    )
+    .join("");
+
+  return galleryItemMerkup;
+}
+
+function getOriginalImg(event) {
+  event.preventDefault();
+  const isImgPrewiewEl = event.target.classList.contains("gallery__image");
+
+  if (!isImgPrewiewEl) {
+    return;
+  }
+  // const imgPreviewEl = event.target;
+  const imgPreviewOriginalURL = event.target.dataset.source;
+  const imgAlt = event.target.getAttribute("alt");
+  // console.log(event.target);
+
+  const modalOriginalImage = basicLightbox.create(`
+    <img src="${imgPreviewOriginalURL}" alt="${imgAlt}"></img>`);
+  modalOriginalImage.show();
+  document.addEventListener("keydown", (event) =>
+    modalCloseOnEscapeKeydown(event, modalOriginalImage)
+  );
+}
+
+function modalCloseOnEscapeKeydown(event, modalOriginalImage) {
+  event.preventDefault();
+  // console.log(event.code);
+  if (event.code !== "Escape") {
+    return;
+  }
+  modalOriginalImage.close();
+  // document.removeEventListener("keydown", (event) => modalCloseOnEscapeKeydown(event, modalOriginalImage));
+}
+
+// Додай закриття модального вікна після натискання клавіші Escape.Зроби так, щоб
+// прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
+
+// Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
---
+++ js/gallery-items.js
@@ -0,0 +1,65 @@
+export const galleryItems = [
+  {
+    preview:
+      'https://pixabay.com/get/g5fc0bcf554af33103e7b9500ae13396f6b4f89cad411bd389ff2538e04054f5b3240c70b91eab66e3082175f147492376151064b3d9fb374f881a6dc73087fe4961b3a386afa1ee7beceeeab90045974_640.jpg',
+    original:
+      'https://pixabay.com/get/g2c95fd606d0f5d60a1b72357fb69d1b0a37e696cca993fe5857f82b71184e7aadb36135dcafaa1b8c8d32da2604b55cbbd30156c2bef8b1bc9f640f893c216b9ff87cc31e360855eac86960140274210_1280.jpg',
+    description: 'Rose Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
